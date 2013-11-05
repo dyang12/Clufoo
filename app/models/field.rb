2c4913aaacc883d86d71f5bca66039f18dbde032
Dean <dean.yang02@gmail.com>
2013-11-05 10:56:47 -0800

    display the fields of the current form on the form show page

--- app/models/field.rb
+++ app/models/field.rb
@@ -5,22 +5,50 @@ class Field
   key :type, String, :required => true
   key :required, String
   key :uniqueness, String
-  key :options, Array
+  key :choices, Array
   
   timestamps!
   
   attr_accessible :label, :type, :required, :uniqueness, :options
   
-  before_validation :generate_options
+  # before_validation :generate_choices
   
-  private
-  def generate_options
-    if self.type == "radio" || "checkbox"
-      default_options = []
-      
-      
-      
-      self.options = default_options
+  def display_type
+    if self.type == "text"
+      return "Single Line Text"
+    elsif self.type == "textarea"
+      return "Paragraph Text"
+    elsif self.type == "checkbox"
+      return "Checkbox"
+    elsif self.type == "select"
+      return "Dropdown"
+    else
+      return "Multiple Choice"
     end
   end
+  
+  def display_options
+    opt_string = ""
+    
+    if self.required
+      opt_string += "Required, "
+    end
+    
+    if self.uniqueness
+      opt_string += "No duplicates, "
+    end
+    
+    opt_string.chop.chop
+  end
+  
+  private
+  # def generate_choices
+#     if self.type == "radio" || "checkbox"
+#       default_choices = []
+#       
+#       
+#       
+#       self.choices = default_choices
+#     end
+#   end
 end

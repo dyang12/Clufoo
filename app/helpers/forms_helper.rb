f6ca66c8da64f756ceb47bbfa6a13c3092c0e6d7
Dean <dean.yang02@gmail.com>
2013-11-05 17:29:47 -0800

    created form from user generated form data

--- app/helpers/forms_helper.rb
+++ app/helpers/forms_helper.rb
@@ -2,4 +2,14 @@ module FormsHelper
   def create_tag(field_id)
     "entry[" + field_id + "]"
   end
+  
+  def get_selections(field)
+    selections = []
+    
+    field.choices.each do |choice|
+      selections << [choice, choice]
+    end
+    
+    selections
+  end
 end
